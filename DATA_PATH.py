ae5f059469645ca386648fe4ace14ecc1dea504e
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-01 11:49:42 +0300

    Full Data Path

--- DATA_PATH.py
+++ DATA_PATH.py
@@ -14,7 +14,7 @@ from PC import pc
 from PC_genrator import PC_gen
 from registers import registers
 from shifter import shifter
-
+from sign_extend import sign_extender
 main_memory = None
 
 
@@ -55,12 +55,15 @@ def top_level(clk, reset, Constant_4):
     ALU_or_load_or_immShiftedBy12 = Signal(intbv(0)[2:])
     Shift_amount = Signal(intbv(0)[2:])
     Enable_Reg = Signal(bool(0))
-    #todo
+    #todo modbv
     input_for_shifter = Signal(modbv(0)[32:])
-    shifter_out = Signal(intbv(0)[32:])
+    #todo modbv
+    shifter_out = Signal(modbv(0)[32:])
     a = Signal(intbv(0)[32:])
     b = Signal(intbv(0)[32:])
     alu_out = Signal(intbv(0)[32:0])
+    sign_selection = Signal(intbv(0)[2:])
+    signed_extnetion_output = Signal(intbv(0)[32:])
 
     # ======================= ins section ======================= #
 
@@ -71,15 +74,15 @@ def top_level(clk, reset, Constant_4):
     mux_PC_or_ALU_to_memory = mux2_1(PC_or_Address, addres, pc_out, alu_out)  # mux PC or ALU to memory
 
     # =================== input == Data in == enable ========== output ===========
-    global main_memory
-    main_memory = memory(addres, rs2_out, enable_write, clk, memory_out, size_sel)  # memory
+    # global main_memory
+    main_memory = memory(rs2_out, enable_write, size_sel, addres, memory_out)
 
     # ================== input =============
     Decode = ins_dec(memory_out, opcode, rd, func3, rs1, rs2, func7, immI, immS, immB, immU, immJ)  # Decoder
     # ================== inputs ========== outputs================== input
     Reg = registers(rs1, rs2, rd, rs1_out, rs2_out, Enable_Reg, data_in_Reg)  # Reg
     ext = extender(immI, immS, immB, immU, immJ, imm32I, imm32S, imm32B, imm32U, imm32J)  # extend for imm
-    mux_Reg = mux_3to1(alu_out, memory_out, shifter_out, ALU_or_load_or_immShiftedBy12, data_in_Reg)  # mux for Reg file
+    mux_Reg = mux_3to1(alu_out, signed_extnetion_output, shifter_out, ALU_or_load_or_immShiftedBy12, data_in_Reg)  # mux for Reg file
     mux_imm = mux8_1(imm32I, imm32S, imm32B, imm32U, imm32J, input_for_shifter, imm_sel)  # mux imm to shift
     # ===============input============== sel========== output
     shift = shifter(input_for_shifter, Shift_amount, shifter_out)  # shifter for imm
@@ -92,15 +95,10 @@ def top_level(clk, reset, Constant_4):
     gen = PC_gen(pc_out, rs1_out, shifter_out, PC_genrator_sel, gen_to_PC)  # PC gen
     cont = control(opcode, func3, func7, alu_out, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
                    rs2_or_imm_or_4,
-                   PC_or_Address, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg)  # Control
+                   PC_or_Address, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg, sign_selection)  # Control
+
+    ins = sign_extender(memory_out, sign_selection, signed_extnetion_output)
     return instances()
-
-@block
-def test_bench():
-
-    # @always(delay(5))
-    # def clk_gen():
-
 
 
 def convert():
