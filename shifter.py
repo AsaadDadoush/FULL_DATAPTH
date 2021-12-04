6c9a7ece218adeafeeb6bffeaa266ae01a8aa1e9
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-04 13:46:21 +0300

    Shift Test

--- shifter.py
+++ shifter.py
@@ -3,7 +3,7 @@ from myhdl import *
 
 @block
 def shifter(data_in, sel, data_out):
-    @always(data_in and sel)
+    @always(data_in, sel)
     def shift():
         if sel == 0:
             data_out.next = data_in.signed() << 1
@@ -13,6 +13,11 @@ def shifter(data_in, sel, data_out):
             data_out.next = data_in.signed()
         else:
             data_out.next = data_in.signed()
+        print("============================ imm Shifter ============================")
+        print("Data in: ", data_in+0)
+        print("Selection: ",sel + 0)
+        print("Data Out: ", data_out.next + 0)
+        print("")
 
     return shift
 
@@ -29,7 +34,23 @@ def test_bench():
         sel.next = 0
         data_in.next = 0b11111111111111111111111111110110
         yield delay(1)
-        print(bin(data_out, 32))
+        print("Data input: ",bin(data_in,32))
+        print("Selection: ", sel + 0)
+        print("Data output: ", bin(data_out, 32))
+        print('-'*50)
+        sel.next = 1
+        data_in.next = 0b11111111111111111111111111110110
+        yield delay(1)
+        print("Data input: ", bin(data_in, 32))
+        print("Selection: ", sel + 0)
+        print("Data output: ", bin(data_out, 32))
+        sel.next = 2
+        data_in.next = 0b11111111111111111111111111110110
+        yield delay(1)
+        print('-' * 50)
+        print("Data input: ", bin(data_in, 32))
+        print("Selection: ", sel + 0)
+        print("Data output: ", bin(data_out, 32))
     return instances()
 
 
