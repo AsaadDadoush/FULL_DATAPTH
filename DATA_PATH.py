0a5faba9481c6f7f7b05e7ef6aaef363410b7eda
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-04 13:44:36 +0300

    extender Test

--- DATA_PATH.py
+++ DATA_PATH.py
@@ -1,30 +1,37 @@
 import struct
 
 from myhdl import *
+
+import mem
+from mem import memory
 from ALU import alu
 from control import control
 from extender import extender
 from Instruction_decoder import ins_dec
-from mem import memory
+from mem import memory, to_number
 from memory import Memory
-from mux2_1 import mux2_1
-from mux3_1 import mux_3to1
+from mux2_1 import mux2_1, mux2_1_pcANDalu
+from mux3_1 import mux_3to1, mux_3to1_for_Register
 from Mux8_1 import mux8_1
 from PC import pc
 from PC_genrator import PC_gen
 from registers import registers
 from shifter import shifter
 from sign_extend import sign_extender
+
+program = Memory()
+program.load_binary_file(path="D:/rarsProject/text.txt", starting_address=0)
+program.load_binary_file(path="D:/rarsProject/data.txt", starting_address=8192)
 main_memory = None
 
 
 @block
-def top_level(clk, reset, Constant_4):
+def top_level(Constant_4):
     # ======================= Lines ======================= #
     gen_to_PC = Signal(intbv(0)[32:])
     pc_out = Signal(intbv(0)[32:])
     addres = Signal(intbv(0)[32:])
-    memory_out = Signal(intbv(0)[32:])
+    memory_out = Signal(intbv(0, min=-2**31, max=2**31))
     rs1 = Signal(intbv(0)[5:0])
     rs2 = Signal(intbv(0)[5:0])
     immU = Signal(intbv(0)[20:0])
@@ -35,7 +42,7 @@ def top_level(clk, reset, Constant_4):
     rd = Signal(intbv(0)[5:])
     rs1_out = Signal(intbv(0)[32:])
     rs2_out = Signal(intbv(0)[32:])
-    data_in_Reg = Signal(intbv(0)[32:])
+    data_in_Reg = Signal(intbv(0, min=-2**31, max=2**31))
     imm32I = Signal(intbv(0)[32:0])
     imm32S = Signal(intbv(0)[32:0])
     imm32B = Signal(intbv(0)[32:0])
@@ -43,9 +50,9 @@ def top_level(clk, reset, Constant_4):
     imm32J = Signal(intbv(0)[32:0])
     opcode = Signal(intbv(0)[7:])
     func3 = Signal(intbv(0)[5:])
-    func7 = Signal(intbv(0)[5:])
-    size_sel = Signal(intbv(0)[2:])
-    operation_sel = Signal(intbv(0)[4:])
+    func7 = Signal(intbv(0))
+    size_sel = Signal(intbv(2)[2:])
+    operation_sel = Signal(intbv(0)[5:])
     enable_write = Signal(bool(0))
     PC_genrator_sel = Signal(intbv(0)[2:])
     imm_sel = Signal(intbv(0)[32:])
@@ -54,35 +61,40 @@ def top_level(clk, reset, Constant_4):
     PC_or_rs1 = Signal(bool(0))
     ALU_or_load_or_immShiftedBy12 = Signal(intbv(0)[2:])
     Shift_amount = Signal(intbv(0)[2:])
-    Enable_Reg = Signal(bool(0))
-    #todo modbv
+    Enable_Reg = Signal(bool(1))
+    # todo modbv
     input_for_shifter = Signal(modbv(0)[32:])
-    #todo modbv
-    shifter_out = Signal(modbv(0)[32:])
-    a = Signal(intbv(0)[32:])
-    b = Signal(intbv(0)[32:])
-    alu_out = Signal(intbv(0)[32:0])
-    sign_selection = Signal(intbv(0)[2:])
-    signed_extnetion_output = Signal(intbv(0)[32:])
+    # todo modbv
+    shifter_out = Signal(intbv(0, min=-2**31, max=2**31))
+    a = Signal(intbv(0, min=-2**31, max=2**31))
+    b = Signal(intbv(0, min=-2**31, max=2**31))
+    alu_out = Signal(intbv(0, min=-2**31, max=2**31))
+    sign_selection = Signal(intbv(2)[2:])
+    signed_extnetion_output = Signal(intbv(0, min=-2**31, max=2**31))
 
     # ======================= ins section ======================= #
 
     # ========= input == out ============
-    PC = pc(gen_to_PC, pc_out, reset, clk)  # PC
+    PC = pc(gen_to_PC, pc_out)  # PC
 
     # =================================== sel ======= out === i0 ==== i1
-    mux_PC_or_ALU_to_memory = mux2_1(PC_or_Address, addres, pc_out, alu_out)  # mux PC or ALU to memory
+    mux_PC_or_ALU_to_memory = mux2_1_pcANDalu(PC_or_Address, addres, pc_out, alu_out)  # mux PC or ALU to memory
 
     # =================== input == Data in == enable ========== output ===========
-    # global main_memory
-    main_memory = memory(rs2_out, enable_write, size_sel, addres, memory_out)
 
+    Main_memory = memory(rs2_out, enable_write, size_sel, addres, memory_out)
     # ================== input =============
     Decode = ins_dec(memory_out, opcode, rd, func3, rs1, rs2, func7, immI, immS, immB, immU, immJ)  # Decoder
+    cont = control(opcode, func3, func7, alu_out, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
+                   rs2_or_imm_or_4,
+                   PC_or_Address, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg,
+                   sign_selection)  # Control
     # ================== inputs ========== outputs================== input
     Reg = registers(rs1, rs2, rd, rs1_out, rs2_out, Enable_Reg, data_in_Reg)  # Reg
+
     ext = extender(immI, immS, immB, immU, immJ, imm32I, imm32S, imm32B, imm32U, imm32J)  # extend for imm
-    mux_Reg = mux_3to1(alu_out, signed_extnetion_output, shifter_out, ALU_or_load_or_immShiftedBy12, data_in_Reg)  # mux for Reg file
+    mux_Reg = mux_3to1_for_Register(alu_out, signed_extnetion_output, shifter_out, ALU_or_load_or_immShiftedBy12,
+                       data_in_Reg)  # mux for Reg file
     mux_imm = mux8_1(imm32I, imm32S, imm32B, imm32U, imm32J, input_for_shifter, imm_sel)  # mux imm to shift
     # ===============input============== sel========== output
     shift = shifter(input_for_shifter, Shift_amount, shifter_out)  # shifter for imm
@@ -93,20 +105,33 @@ def top_level(clk, reset, Constant_4):
     ALU = alu(a, b, operation_sel, alu_out)  # ALU
     # ======================== inputs ============= sel ========= out
     gen = PC_gen(pc_out, rs1_out, shifter_out, PC_genrator_sel, gen_to_PC)  # PC gen
-    cont = control(opcode, func3, func7, alu_out, size_sel, operation_sel, enable_write, PC_genrator_sel, imm_sel,
-                   rs2_or_imm_or_4,
-                   PC_or_Address, PC_or_rs1, ALU_or_load_or_immShiftedBy12, Shift_amount, Enable_Reg, sign_selection)  # Control
 
-    ins = sign_extender(memory_out, sign_selection, signed_extnetion_output)
+    sign_ex = sign_extender(memory_out, sign_selection, signed_extnetion_output)
+
+    return instances()
+
+
+enable_PC = Signal(bool(0))
+loading_flag = Signal(bool(0))
+load_data = Signal(intbv(0)[32:])
+
+
+@block
+def test_bench():
+    Constant_4 = Signal(intbv(4)[32:])
+    ins = top_level(Constant_4)
     return instances()
 
 
 def convert():
-    clk = Signal(bool(0))
-    reset = ResetSignal(0, active=1, isasync=True)
     Constant_4 = Signal(intbv(0)[32:])
-    ins = top_level(clk, reset, Constant_4)
+    ins = top_level(Constant_4)
     ins.convert(hdl='Verilog')
 
 
-convert()
+
+# convert()
+test = test_bench()
+test.run_sim()
+
+
