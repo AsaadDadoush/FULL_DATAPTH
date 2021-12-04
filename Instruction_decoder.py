5af4eeb099119e02e09e57aba98f5a24fc46b418
AsaadDadoush <asaaddaadouch246@gmail.com>
2021-12-04 13:45:43 +0300

    Decoder Test

--- Instruction_decoder.py
+++ Instruction_decoder.py
@@ -4,18 +4,17 @@ from myhdl import *
 @block
 def ins_dec(data_in, opcode, rd, func3, rs1, rs2, func7, immI, immS, immB, immU, immJ):
 
-    @always_comb
+    @always(data_in)
     def decoder():
         opcode.next = data_in[7:0]  # opcode
         func3.next = data_in[15:12]  # func3
-        func7.next = data_in[32:25]  # func7
+        func7.next = data_in[32:25] # func7
         rd.next = data_in[12:7]  # rd
         rs1.next = data_in[20:15]  # rs1
         rs2.next = data_in[25:20]  # rs2
 
         # I-type
         immI.next = intbv(data_in[32:20])
-
         # S-type
         immS.next = intbv(concat(data_in[32:25], data_in[12:7]))
 
@@ -27,6 +26,21 @@ def ins_dec(data_in, opcode, rd, func3, rs1, rs2, func7, immI, immS, immB, immU,
 
         # J-Type
         immJ.next = intbv(concat(data_in[31], data_in[20:12], data_in[20], data_in[31:21]))
+        print("======================== Instruction decoder ========================")
+        print("--------------- input ---------------")
+        print("Data in: ", bin(data_in,32), " = ", data_in+0)
+        print("-------------- Outputs --------------")
+        print("Opcode: ", bin(opcode.next, 7))
+        print("func3: ", bin(func3.next, 3), " = ", func3.next+0)
+        print("func7: ", bin(func7.next, 7), " = ", func7.next+0)
+        print("immI: ", bin(immI.next, 12), " = ", immI.next + 0)
+        print("immS: ", bin(immS.next, 12), " = ", immS.next + 0)
+        print("immB: ", bin(immB.next, 12), " = ", immB.next + 0)
+        print("immU: ", bin(immU.next, 20), " = ", immU.next + 0)
+        print("immJ: ", bin(immJ.next, 20), " = ", immJ.next + 0)
+        print("")
+
+
 
     return decoder
 
